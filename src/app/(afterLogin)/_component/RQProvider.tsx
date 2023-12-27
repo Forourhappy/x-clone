69844b63696e0b857a296f8577452c13d23ab9eb
Forourhappy <foroh1504@gmail.com>
2023-12-28 02:23:39 +0900

    feat: react-query 세팅

---
+++ src/app/(afterLogin)/_component/RQProvider.tsx
@@ -0,0 +1,26 @@
+import { ReactNode, useState } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const RQProvider = ({ children }: { children: ReactNode }) => {
+  const [client] = useState(
+    new QueryClient({
+      defaultOptions: {
+        queries: {
+          refetchOnWindowFocus: false,
+          retry: false,
+        },
+      },
+    }),
+  );
+  return (
+    <QueryClientProvider client={client}>
+      {children}
+      <ReactQueryDevtools
+        initialIsOpen={process.env.NEXT_PUBLIC_MODE === 'local'}
+      />
+    </QueryClientProvider>
+  );
+};
+
+export default RQProvider;
